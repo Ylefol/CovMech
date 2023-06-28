f21b97d8cf0b910297a4270c371af75136ca2dca
Yohan Lefol <yohan.lefol@gmail.com>
2023-06-28 11:20:28 +0200

    Small script update

--- scripts/Indiv_scripts/covid_article_plots.R
+++ scripts/Indiv_scripts/covid_article_plots.R
@@ -1,31 +1,31 @@
 #Custom figures for CoV-Mech article
-# setwd('~/A_Projects/EpiGen/R_Work_Folder/Cov_Mech/')
+setwd('A_Projects/EpiGen/R_Work_Folder/CovMech/')
 #Dotplots for figure 1D/2 – merger of timepoints
 # load('results_folder/critical/cluster_profiler_results/severe.cov.T1_vs_moderate.cov.T1/cluster_profiler_objects.RData')
-load('results_folder/critical/cluster_profiler_results/all.cov.T1_vs_healthy.controls///cluster_profiler_objects.RData')
-# load('results_folder/viremia/cluster_profiler_results/viremia.cov.D1_vs_non_viremia.cov.D1/cluster_profiler_objects.RData')
+# load('results_folder/critical/cluster_profiler_results/all.cov.T1_vs_healthy.controls///cluster_profiler_objects.RData')
+load('results_folder/viremia/cluster_profiler_results/viremia.cov.D1_vs_non_viremia.cov.D1/cluster_profiler_objects.RData')
 
 TP1_gse<-gse
 # load('results_folder/critical/cluster_profiler_results/severe.cov.T2_vs_moderate.cov.T2/cluster_profiler_objects.RData')
-load('results_folder/critical/cluster_profiler_results/all.cov.T2_vs_healthy.controls////cluster_profiler_objects.RData')
-# load('results_folder/viremia/cluster_profiler_results/viremia.cov.D3_vs_non_viremia.cov.D3/cluster_profiler_objects.RData')
+# load('results_folder/critical/cluster_profiler_results/all.cov.T2_vs_healthy.controls////cluster_profiler_objects.RData')
+load('results_folder/viremia/cluster_profiler_results/viremia.cov.D3_vs_non_viremia.cov.D3/cluster_profiler_objects.RData')
 
 TP2_gse<-gse
 # load('results_folder/critical/cluster_profiler_results/severe.cov.T3_vs_moderate.cov.T3//cluster_profiler_objects.RData')
-load('results_folder/critical/cluster_profiler_results/all.cov.T3_vs_healthy.controls////cluster_profiler_objects.RData')
-# load('results_folder/viremia/cluster_profiler_results/viremia.cov.D8_vs_non_viremia.cov.D8/cluster_profiler_objects.RData')
+# load('results_folder/critical/cluster_profiler_results/all.cov.T3_vs_healthy.controls////cluster_profiler_objects.RData')
+load('results_folder/viremia/cluster_profiler_results/viremia.cov.D8_vs_non_viremia.cov.D8/cluster_profiler_objects.RData')
 
 TP3_gse<-gse
 
 rm(gene_list,go_enrich_down,go_enrich_up,gse)
 source('R/my_ClusterProfiler_functions.R')
 
-custom_GO_selection<-c('neutrophil mediated immunity','neutrophil degranulation','type I interferon signaling pathway',
+custom_GO_selection<-c('neutrophil degranulation','neutrophil activation','type I interferon signaling pathway',
                        'response to type I interferon','response to interferon-gamma','interferon-gamma-mediated signaling pathway',
                        'defense response to virus','cytokine-mediated signaling pathway','cytokine production',
-                       'wound healing','coagulation','platelet degranulation','platelet activation','humoral immune response',
-                       'RNA catabolic process','translation','ribosome biogenesis','antigen processing and presentation of peptide antigen via MHC class II',
-                       'MHC class II protein complex assembly','T cell receptor signaling pathway')
+                       'wound healing','coagulation','platelet activation','humoral immune response',
+                       'translation','ribosome biogenesis','antigen processing and presentation of peptide antigen via MHC class II',
+                       'MHC class II protein complex assembly','T cell receptor signaling pathway','T cell activation')
 #Need to build a df with everything, use rbind to slap them together
 #at their different timepoints.
 #Start by getting the top 10 of both positively and negatively enriched
@@ -111,12 +111,12 @@ plt<-ggplot(full_data, aes(x=timepoints, y=Description, size=`-log10(padj)`, col
                        breaks=c(-1,0,1),
                        # breaks=c(1,0.80,0.60,0.40,0.20, 0,-0.20,-0.40,-0.60,-0.80,-1),
                        limits=c(-1, 1), oob=squish,
-                       guide=guide_colorbar(reverse=F), name = 'enrichmentScore',aesthetics = "colour")+ 
+                       guide=guide_colorbar(reverse=F), name = 'enrichmentScore',aesthetics = "colour")+
   # scale_color_continuous(low="blue", high="gray", name = color,
   #                        limits=c(-1, 0),
   #                        guide='legend') +
   scale_y_discrete(labels = function(x) str_wrap(str_replace_all(x, "foo" , " "), width = 50)) +
-  ylab(NULL) + ggtitle('Critical vs Mopderate') + theme_dose(15) +
+  ylab(NULL) + ggtitle('Viremia vs non-viremia') + theme_dose(15) +
   scale_size(range=c(3, 8))
 
 ggsave(file='merged_tp_dotplot.png',plt,width = 10, height = 8,dpi=300)
@@ -127,10 +127,10 @@ ggsave(file='merged_tp_dotplot.png',plt,width = 10, height = 8,dpi=300)
 #Plot original C6 genes for the bad mech vent group
 
 # setwd("~/A_Projects/EpiGen/R_Work_Folder/TS_dev_zone")
-# 
+#
 # og_cmap<-read.csv('Results_folder/covid_results/TS_cov_results/PART_results/PART_heat_cmap.csv',row.names=1)
 # new_cmap<-og_cmap[og_cmap$cluster=='C6',]
-# 
+#
 # load('Results_folder/covid_results/TS_results_rem_bad_mechvent/timeseries_obj_res.Rdata')
 # source('R/DE_PART_results_functions.R')
 # my_ts_data<-calculate_cluster_traj_data(TS_object,custom_cmap = new_cmap,scale_feat=T)
@@ -152,21 +152,21 @@ ggsave(file='merged_tp_dotplot.png',plt,width = 10, height = 8,dpi=300)
 #custom PART plot function
 source('R/DE_PART_results_functions.R')
 PART_heat_map_custom<-function(object, heat_name='custom_heat_map'){
-  
+
   clust_ordered <- unique(as.character(object@PART_results$cluster_info[['calculated_clusters']]$lab.hatK[object@PART_results$cluster_info[['clustered_rows']]$order]))
   #Cluster illustration stored as rowAnnotation
-  row_annot <- ComplexHeatmap::rowAnnotation(gene_cluster = as.character(object@PART_results$cluster_info[['calculated_clusters']]$lab.hatK), 
+  row_annot <- ComplexHeatmap::rowAnnotation(gene_cluster = as.character(object@PART_results$cluster_info[['calculated_clusters']]$lab.hatK),
                                              col = list(gene_cluster=object@PART_results$cluster_info[['colored_clust_rows']]),
                                              show_annotation_name=F,
                                              annotation_legend_param = list(title = "clusters", at = clust_ordered,
                                                                             labels = unique(object@PART_results$cluster_map$cluster)))
-  
+
   #Create top annotations
   top_annot_results<-prepare_top_annotation_PART_heat(object)
   top_annot_labels<-top_annot_results[[1]]
   top_annot_no_labels<-top_annot_results[[2]]
   col_split<-top_annot_results[[3]]
-  
+
   #Prepare a 'gap_vect' to split the different elements of the heatmap
   col_split_vect<-levels(col_split)
   gap_vect<-c()
@@ -181,16 +181,16 @@ PART_heat_map_custom<-function(object, heat_name='custom_heat_map'){
       }
     }
   }
-  
+
   #Create the legend for the groups
-  lgd = Legend(labels = names(object@group_colors), title = "groups", 
+  lgd = Legend(labels = names(object@group_colors), title = "groups",
                legend_gp = gpar(fill = unname(object@group_colors)))
 
   #Plot the heatmap
   reso <- 300
   length <- 3.25*reso/72
   width<-length-(length/4)
-  
+
   png(paste0(heat_name,".png"),units='in',width=width,height=length,res = reso)
   draw(
     ComplexHeatmap::Heatmap(
@@ -221,43 +221,43 @@ PART_heat_map_custom(TS_object)
 # library("xlsx")
 # file_locations<-'TS_results/gprofiler_results/data_files/'
 # excel_file_name<-'TS_GOs.xlsx'
-# 
+#
 # for(item in list.files(file_locations)){
 #   name<-strsplit(item,'_data.csv')[[1]]
 #   print(name)
 #   my_dta<-read.csv(paste0(file_locations,item),row.names = 1)
 #   my_dta<-my_dta[,c('term_id','term_name','p_value','term_size','intersection_size')]
-#   
+#
 #   if(excel_file_name %in% list.files()==F){
-#     write.xlsx(my_dta, excel_file_name, sheetName = name, 
+#     write.xlsx(my_dta, excel_file_name, sheetName = name,
 #                col.names = T, row.names = T, append = F)
 #   }else{
-#     write.xlsx(my_dta, excel_file_name, sheetName = name, 
+#     write.xlsx(my_dta, excel_file_name, sheetName = name,
 #                col.names = T, row.names = T, append = T)
 #   }
 # }
 
-# 
-# 
+#
+#
 # library("xlsx")
 # file_locations<-'WGCNA_results/gprofiler_results/data_files/'
 # excel_file_name<-'WGCNA_GOs.xlsx'
-# 
+#
 # for(item in list.files(file_locations)){
 #   name<-strsplit(item,'_data.csv')[[1]]
 #   print(name)
 #   my_raw_dta<-read.csv(paste0(file_locations,item))
 #   my_dta<-my_raw_dta[my_raw_dta$source=='GO:BP',]
 #   my_dta<-my_dta[,c('term_id','term_name','p_value','term_size','intersection_size')]
-#   
+#
 #   if(nrow(my_dta)>0){
 #     if(excel_file_name %in% list.files()==F){
-#       write.xlsx(my_dta, excel_file_name, sheetName = name, 
+#       write.xlsx(my_dta, excel_file_name, sheetName = name,
 #                  col.names = T, row.names = F, append = F)
 #     }else{
-#       write.xlsx(my_dta, excel_file_name, sheetName = name, 
+#       write.xlsx(my_dta, excel_file_name, sheetName = name,
 #                  col.names = T, row.names = , append = T)
 #     }
 #   }
-# 
+#
 # }
